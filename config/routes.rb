1f7ff33189a7eb02ba5b6e114dfb8bbacf0481ec
jkr <abduqahhor.muhamadjonov@gmail.com>
2018-02-14 11:10:51 +0500

    sign up user from UI is done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   resources :articles  #this gives a resource to create, delete, patch,.... articles.
 
-
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
