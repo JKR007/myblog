5c0ce9e9a049c7ade5b34be94fb62355bbef1da5
jkr <abduqahhor.muhamadjonov@gmail.com>
2018-02-15 12:50:40 +0500

    into users Admin added and control over all articles and deleting users given to admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
+  # => <dependent: :destroy> => if user is deleted all dependent articles to user also be deleted
   before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
