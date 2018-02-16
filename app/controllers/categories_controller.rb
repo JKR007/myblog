310921d1a75aa20a3410d55f7f9ca1f22ed3f9a3
jkr <abduqahhor.muhamadjonov@gmail.com>
2018-02-16 14:11:58 +0500

    nothing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,7 +32,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:success] = "Category name was successfully updated"
-      redirect_to categories_path
+      redirect_to categories_path(@category)
     else
       redirect_to categories_path
     end
